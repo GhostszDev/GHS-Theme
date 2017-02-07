7fbd2e18a10e19b2cd018a21caa30d2ee95d6021
Steven Rivera <ghostszmusic@gmail.com>
2017-02-07 11:42:56 -0500

    added login and signup function calls to angularJS

--- js/GhsJS.js
+++ js/GhsJS.js
@@ -1,60 +1,58 @@
 angular.module('GHS_mod', ['ngRoute'])
-    .controller('GHS_ctrl',
-        function($scope, $http){
+    .controller('GHS_ctrl', function($scope, $http) {
 
-            //params
-            $scope.user = [];
+        //params
+        $scope.user = [];
 
-            //login function
-            $scope.login = function(user){
+        //login function
+        $scope.login = function (user) {
 
-                var param = {
-                    user_login: user.name,
-                    user_password: user.password,
-                    remember: user.remember
-                };
-
-                $http.post('http://ghostszmusic.dev/wp-json/ghs_api/v1/login', param)
-                    .then(function(data){
-
-                        if(data.success){
-                            console.log('Success!');
-                        } else {
-                            console.log(data.error_message);
-                        }
-
-                    })
-                    .catch(function(){
-
-                    });
+            var param = {
+                user_login: user.name,
+                user_password: user.password,
+                remember: user.remember
             };
 
-            //signup function
-            $scope.signup = function(user){
+            $http.post('http://ghostszmusic.dev/wp-json/ghs_api/v1/login', param)
+                .then(function (data) {
 
-                var param = {
-                    user_name: user.name,
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    email: user.email,
-                    password: user.password,
-                    mailing: user.mailing
-                };
+                    if (data.success) {
+                        console.log('Success!');
+                    } else {
+                        console.log(data.error_message);
+                    }
 
-                $http.post('http://ghostszmusic.dev/wp-json/ghs_api/v1/signup', param)
-                    .then(function(data){
+                })
+                .catch(function () {
 
-                        if(data.success){
-                            console.log('Success!');
-                        } else {
-                            console.log(data.error_message);
-                        }
+                });
+        };
 
-                    })
-                    .catch(function(){
+        //signup function
+        $scope.signup = function (user) {
 
-                    });
-            }
+            var param = {
+                user_name: user.name,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email,
+                password: user.password,
+                mailing: user.mailing
+            };
 
-        }
-    );
+            $http.post('http://ghostszmusic.dev/wp-json/ghs_api/v1/signup', param)
+                .then(function (data) {
+
+                    if (data.success) {
+                        console.log('Success!');
+                    } else {
+                        console.log(data.error_message);
+                    }
+
+                })
+                .catch(function () {
+
+                });
+        };
+
+    });
