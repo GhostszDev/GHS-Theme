60753b7e4d9ef399a1eae118c477d95d181de17c
Steven Rivera <ghostszmusic@gmail.com>
2017-02-07 12:02:06 -0500

    localization of scripts

--- functions.php
+++ functions.php
@@ -15,7 +15,11 @@ function wpdocs_theme_name_scripts() {
     wp_enqueue_script('angularjs', get_stylesheet_directory_uri() . '/js/angular.min.js');
     wp_enqueue_script('angularjs-resource', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular-resource.min.js');
     wp_enqueue_script('angularjs-route', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular-route.min.js');
-    wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/GhsJS.js', array( 'angularjs', 'angularjs-route'), true);
+    wp_enqueue_script('GhsJS', get_stylesheet_directory_uri() . '/js/GhsJS.js', array( 'angularjs', 'angularjs-route'), true);
+
+    //localization of scripts
+    wp_localize_script('GhsJS', 'myLocalized', array('partials' => trailingslashit( get_template_directory_uri() ) . 'partials/'));
 }
 
+//adding actions
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
