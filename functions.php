54cbd47d3ff1dd76528f27d6dae8844c5a7c7054
Steven Rivera <ghostszmusic@gmail.com>
2017-02-08 09:48:13 -0500

    added js

--- functions.php
+++ functions.php
@@ -19,7 +19,10 @@ function wpdocs_theme_name_scripts() {
     wp_enqueue_script('angularjs', get_stylesheet_directory_uri() . '/js/angular.min.js');
     wp_enqueue_script('angularjs-resource', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular-resource.min.js');
     wp_enqueue_script('angularjs-route', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular-route.min.js');
-    wp_enqueue_script('GhsJS', get_stylesheet_directory_uri() . '/js/GhsJS.js', array( 'angularjs', 'angularjs-route'), true);
+    wp_enqueue_script('jq', get_stylesheet_directory_uri() . '/js/jquery-2.1.3.js');
+    wp_enqueue_script('main', get_stylesheet_directory_uri() . '/js/main.js');
+    wp_enqueue_script('app', get_stylesheet_directory_uri() . '/js/app.js');
+    wp_enqueue_script('GhsJS', get_stylesheet_directory_uri() . '/js/GhsJS.js', array( 'angularjs', 'angularjs-route', 'jq', 'main', 'app'), true);
 
     //localization of scripts
     wp_localize_script('GhsJS', 'myLocalized', array('partials' => trailingslashit( get_template_directory_uri() ) . 'partials/'));
