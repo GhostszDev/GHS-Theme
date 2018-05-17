b3a0cc73f3d524e7c331f021ca5373862d9de690
Steven Rivera <ghostszmusic@gmail.com>
2018-05-16 21:43:40 -0400

    fixing oauth token

--- page-appData.php
+++ page-appData.php
@@ -1,5 +1,24 @@
 <?php
 
-wp_remote_post('http://localhost/wp-json/ghs_api/v1/socialStats', $args = [
-    'instaCode' => 'instaCode'
-]);
+$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
+if (!isset($_GET['code']))
+{
+    $auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI);
+    header('Location: ' . $auth_url);
+    die('Redirect');
+}
+else {
+    $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
+    $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
+    echo "<pre>";
+    print_r(json_encode($response));
+}
+
+$instaCode = $_GET['instaCode'];
+
+if($instaCode) {
+
+    wp_remote_post('http://localhost/wp-json/ghs_api/v1/socialStats', $args = [
+        'instaCode' => 'instaCode'
+    ]);
+}
